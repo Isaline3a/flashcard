bd5563fc9ed412fac8168296e145c3f3e7bd7000
Isaline3a <00001097100285sp@al.educacao.sp.gov.br>
2024-11-27 12:12:00 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'História',
+    'Qual a origem da expressão a cobra vai fumar?',
+    'O slogan foi uma resposta à descrente opinião pública da época, que dizia que era mais fácil uma cobra fumar do que o Brasil entrar na guerra.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'filosofia',
+    'De quem é a famosa frase "penso, logo existo?',
+    'A frase é do filósofo Descartes'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Qual a tradução da frase "its made me dizzy" ?',
+    'isso me deixou tonto'
+)
